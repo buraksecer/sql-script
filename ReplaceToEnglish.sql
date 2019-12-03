42f09944dd587c026a9e314a932712f36db80ff4
burak seçer <buraksecer@users.noreply.github.com>
2019-12-03 17:18:23 +0300

    W/ Yakup Akmercan

---
+++ ReplaceToEnglish.sql
@@ -0,0 +1,5 @@
+﻿CREATE FUNCTION ReplaceToEnglish(@StringWord AS NVARCHAR(500))
+RETURNS NVARCHAR(500)
+BEGIN
+RETURN  REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(LOWER(@StringWord),'ğ','g'),'ü','u'),'ı','i'),'ş','s'),'ö','o'),'ç','c')  
+END 
